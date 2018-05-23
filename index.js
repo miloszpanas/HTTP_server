e61bf92f8673660edf52882395ee7dd0cc52508c
Miłosz Panas <miloszpanas@gmail.com>
2018-05-23 22:01:40 +0200

    Add fsread file to show image from the drive

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
         if (request.method === 'GET' && request.url === '/') {
             fs.readFile('./html/index.html', 'utf-8', function(err, data) {
+                if (err) throw err;
                 response.write(data);
                 response.end();
             })
             } else {
-                response.statusCode = 404;
-                response.write('<img src="https://i.ytimg.com/vi/2qZHh_iN5Zs/hqdefault.jpg" style="display:block;margin:auto;width:40%;" alt="doggo">');
-                response.write('<h2 style="text-align:center;">Wrong way, dude</h2>');
-                response.end();
-            }
-        });
+                    response.statusCode = 404;
+                    fs.readFile('./doggo.jpg', 'utf-8', function(err, data) {
+                        if (err) throw err;
+                        response.write(data);
+                        response.write('<h2 style="text-align:center;">Wrong way, dude</h2>');
+                        response.end();
+                    })
+                }
+            });
 
 server.listen(8080);
